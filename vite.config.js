0d56961529562b8c66a1021a24ae03468c4dcd3e
Laslo89 <matze_lebt@gmx.de>
2022-10-24 12:56:47 +0200

    fix: add relative paths for assets

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '', //makes paths relative
   root: "src",
-  assetsInclude: ["./assets/**/*.*"],
+  assetsInclude: ["/assets/**/*.*"],
   build: {
     emptyOutDir: true,
     outDir: "../dist"
-  }
+  },
 })
